f84a9b4984c3acc5621a1c82ee6a6318d6dfb30d
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2025-08-13 06:00:23 -0500

    The server side of it

--- examples/appsync/server.cpp
+++ examples/appsync/server.cpp
@@ -6,32 +6,12 @@
 
 #include "socket.h"
 #include "http/router.h"
-#include "tls/security.h"
+#include "dtls/security.h"
 #include "server.h"
 
 #include "common.h"
 
 using namespace netpp;
-
-struct MessagePacket {
-  size_t m_client_name_len;
-  size_t m_message_len;
-
-  bool validate(uint32_t packet_len) const {
-    if (m_client_name_len == 0 || m_message_len == 0) {
-      return false;
-    }
-    return packet_len >= sizeof(MessagePacket) + m_client_name_len + m_message_len + 2;
-  }
-
-  char* get_client_name() const {
-    return (char*)(this + 1);
-  }
-
-  char* get_message() const {
-    return get_client_name() + m_client_name_len + 1;
-  }
-};
 
 int main(int argc, char** argv) {
   if (!sockets_initialize()) {
@@ -46,13 +26,13 @@ int main(int argc, char** argv) {
   }
 
 #if SERVER_USE_TLS
-  TLSSecurityFactory* security = new TLSSecurityFactory(true, SERVER_KEY, SERVER_CERT, "", "localhost", SERVER_CERT_PASSWD,
+  DTLSSecurityFactory* security = new DTLSSecurityFactory(true, SERVER_KEY, SERVER_CERT, "", "localhost", SERVER_CERT_PASSWD,
     ETLSVerifyFlags::VERIFY_PEER);
 #else
-  TLSSecurityFactory* security = nullptr;
+  DTLSSecurityFactory* security = nullptr;
 #endif
 
-  TCP_Server server(security, 1024);
+  UDP_Server server(security, 1024);
 
   if (server.start(SERVER_IPV4, SERVER_PORT)) {
     printf("Server started on %s:%s\n", server.hostname().c_str(), server.port().c_str());
@@ -62,58 +42,36 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  HTTP_Router router;
 
-  router.on_get("/history", [](const HTTP_Request* request) -> HTTP_Response* {
-    std::ifstream history_file("./_chat_history.txt");
-    if (history_file.is_open()) {
-      std::string history_content((std::istreambuf_iterator<char>(history_file)), std::istreambuf_iterator<char>());
-
-      HTTP_Response* response = HTTP_Response::create(EHTTP_ResponseStatusCode::E_STATUS_OK);
-      response->set_version("1.1");
-      response->add_header("Content-Type: text/plain; charset=UTF-8");
-      response->add_header("Connection: keep-alive");
-      response->set_body(history_content);
-      return response;
-    }
-
-    HTTP_Response* response = HTTP_Response::create(EHTTP_ResponseStatusCode::E_STATUS_NOT_FOUND);
-    response->set_version("1.1");
-    return response;
-    });
-
-  router.on_unhandled([](const HTTP_Request* request) -> HTTP_Response* {
-    HTTP_Response* response = HTTP_Response::create(EHTTP_ResponseStatusCode::E_STATUS_METHOD_NOT_ALLOWED);
-    response->set_version("1.1");
-    response->add_header("Content-Type: text/plain; charset=UTF-8");
-    response->add_header("Connection: keep-alive");
-    return response;
-    });
-
-  server.on_http_request([&router](const ISocketPipe* source, const HTTP_Request* request) {
-    return router.signal_method(request);
-    });
-
+  SyncServerPacket pckt_state = {};
 
   server.on_raw_receive([&](const ISocketPipe* source, const RawPacket* packet) {
-    MessagePacket* msg = (MessagePacket*)packet->message();
-    if (!msg->validate(packet->length())) {
-      fprintf(stderr, "Invalid message packet received\n");
-      return nullptr;
+    SyncClientPacket* msg = (SyncClientPacket*)packet->message();
+
+    bool updated = false;
+    for (size_t i = 0; i < pckt_state.m_peer_count; ++i) {
+      if (pckt_state.m_peer_infos[i].m_peer_id == msg->m_peer_id) {
+        pckt_state.m_peer_infos[i] = msg;
+        updated = true;
+        break;
+      }
     }
 
-    const char* client_name = msg->get_client_name();
-    const char* message = msg->get_message();
+    // No logic for removing peers currently
+    if (!updated) {
+      pckt_state.m_peer_infos[pckt_state.m_peer_count++] = msg;
+    }
 
-    history_file << "Client (" << client_name << "): " << message << "\n";
-    history_file.flush();
-
-    printf("Client (%s): %s\n", client_name, message);
-    server.send_all(packet);
     return nullptr;
     });
 
-  while (server.is_running()) { std::this_thread::sleep_for(std::chrono::seconds(1)); }
+  while (server.is_running()) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    
+    RawPacket* pckt = RawPacket::create((void*)pckt_state, sizeof(pckt_state));
+    server.send_all(pckt);
+    delete pckt;
+  }
 
   delete security;
 
