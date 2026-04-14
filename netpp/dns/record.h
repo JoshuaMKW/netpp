324491fd9b69f8bc1fb51ab987c222b794bf69b6
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2026-04-14 15:17:05 -0500

    Cute outline for now

--- netpp/dns/record.h
+++ netpp/dns/record.h
@@ -7,16 +7,38 @@
 
 namespace netpp {
 
-    class DNS_Record {
-
+    class DNS_Question {
+    public:
+        friend class DNS_Message;
+        // ...
     };
 
-    class DNS_Query : public DNS_Record {
-
+    class DNS_Answer {
+    public:
+        friend class DNS_Message;
+        // ...
     };
 
-    class DNS_Response : public DNS_Record {
+    class DNS_Message {
+    public:
+        DNS_Message() = delete;
+        DNS_Message(const DNS_Message&) = default;
+        DNS_Message(DNS_Message&&) noexcept = default;
 
+        static bool is_data_query(const char* msg_buf, uint32_t buf_size);
+        static bool is_data_response(const char* msg_buf, uint32_t buf_size);
+
+        static DNS_Message* create_query();
+        static DNS_Message* create_response();
+
+        static const char* build_buf(const DNS_Message& msg, uint32_t* size_out);
+
+        const std::vector<DNS_Question>& questions() const { return m_questions; }
+        const std::vector<DNS_Answer>& answers() const { return m_answers; }
+
+    private:
+        std::vector<DNS_Question> m_questions;
+        std::vector<DNS_Answer> m_answers;
     };
 
 }
